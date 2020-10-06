4bace00f73ff7cc5dc610d779ec37f8693edc0fb
Katherine Allen <katherineallen067@gmail.com>
2020-10-06 17:28:31 -0400

    big o question 2

--- script.js
+++ script.js
@@ -11,3 +11,16 @@ int product(int a, int b){
 //this is linear time because it touches all nodes
 // comparing a and b
 // runtime would = 0(ab) 
+
+int power(int a, int b){
+    if (b < 0){
+        return 0; 
+    }else  if (b ==0){
+        return 1;
+    }else{
+        return a * power(a, b-1);
+    }
+}
+//need to find runtime of a to the power of b
+//this is recursion from b to b-1 to b-2 down to 1 
+// runtime would be 0(n)
